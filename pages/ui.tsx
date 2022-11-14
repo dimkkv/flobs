63df966744425b13d19ade7dc11fd4c5516b02dd
Dima v <dima@immuna.xyz>
2022-11-14 20:24:48 +0300

    chore: wip ui

---
+++ pages/ui.tsx
@@ -0,0 +1,13 @@
+import Drawer from 'components/drawer';
+import Table from 'components/table';
+import React from 'react';
+
+function Ui() {
+  return (
+    <Drawer>
+      <Table />
+    </Drawer>
+  );
+}
+
+export default Ui;
